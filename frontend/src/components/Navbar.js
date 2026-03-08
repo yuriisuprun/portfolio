c6d4ae9464bc274e11c090887083ca2940023815
yurii-suprun <iursuprun@gmail.com>
2026-03-08 19:21:46 +0100

    Implement navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import ThemeToggle from "./ThemeToggle";
+
+export default function Navbar(){
+
+    return(
+
+        <nav className="flex justify-between p-6 border-b">
+
+            <h1 className="font-bold text-xl">
+                DevPortfolio
+            </h1>
+
+            <div className="flex gap-6">
+
+                <a href="#home">Home</a>
+                <a href="#projects">Projects</a>
+
+                <ThemeToggle/>
+
+            </div>
+
+        </nav>
+
+    )
+
+}
