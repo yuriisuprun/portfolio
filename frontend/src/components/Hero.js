1bde4dfa55cf79bae103948b836d33c1639e3ec2
yurii-suprun <iursuprun@gmail.com>
2026-03-09 18:23:05 +0100

    Add hero component

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -7,35 +7,26 @@ export default function Hero() {
                 <p className="text-xl">Software engineer</p>
 
                 <div className="flex items-center gap-6 mt-4">
-                    <a
-                        href="https://github.com/yuriisuprun"
+                    <a href="https://github.com/yuriisuprun"
                         title="Github"
                         target="_blank"
                         rel="noreferrer"
-                        className="hover:scale-110 transition"
-                    >
-                        <img src="/icons/github.png" alt="GitHub" className="w-6 h-6" />
-                    </a>
+                        className="hover:scale-110 transition">
+                        <img src="/icons/github.png" alt="GitHub" className="w-6 h-6" /></a>
 
-                    <a
-                        href="https://www.linkedin.com/in/yurii-suprun/"
+                    <a href="https://www.linkedin.com/in/yurii-suprun/"
                         title="Linkedin"
                         target="_blank"
                         rel="noreferrer"
-                        className="hover:scale-110 transition"
-                    >
-                        <img src="/icons/linkedin.png" alt="LinkedIn" className="w-6 h-6" />
-                    </a>
+                        className="hover:scale-110 transition">
+                        <img src="/icons/linkedin.png" alt="LinkedIn" className="w-6 h-6" /></a>
 
-                    <a
-                        href="mailto:iursuprun@gmail.com"
+                    <a href="mailto:iursuprun@gmail.com"
                         title="Email"
                         target="_blank"
                         rel="noreferrer"
-                        className="hover:scale-110 transition"
-                    >
-                        <img src="/icons/email.png" alt="Email" className="w-6 h-6" />
-                    </a>
+                        className="hover:scale-110 transition">
+                        <img src="/icons/email.png" alt="Email" className="w-6 h-6" /></a>
                 </div>
             </div>
         </section>
