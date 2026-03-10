6726d8594361c3c78622db3ad912b1a7f5490b00
yurii-suprun <iursuprun@gmail.com>
2026-03-10 13:51:22 +0100

    Update contacts component

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -32,31 +32,24 @@ export default function Contacts({ language }) {
                 </p>
 
                 <div className="flex flex-col gap-6 text-lg">
-                    Email: iursuprun@gmail.com
-                    {/*<a*/}
-                    {/*    href="mailto:iursuprun@gmail.com"*/}
-                    {/*    className="flex items-center justify-center gap-3 hover:underline"*/}
-                    {/*>*/}
-                    {/*    <img src="/icons/email.png" alt="email" className="w-6 h-6 filter dark:invert"/>*/}
-                    {/*    {t.email}*/}
-                    {/*</a>*/}
+                    <a href="mailto:iursuprun@gmail.com"
+                        className="flex items-center justify-center gap-3 hover:underline">
+                        <img src="/icons/email.png" alt="email" className="w-6 h-6 filter dark:invert"/>
+                        iursuprun@gmail.com
+                    </a>
 
-                    <a
-                        href="https://www.linkedin.com/in/yurii-suprun/"
+                    <a href="https://www.linkedin.com/in/yurii-suprun/"
                         target="_blank"
                         rel="noreferrer"
-                        className="flex items-center justify-center gap-3 hover:underline"
-                    >
+                        className="flex items-center justify-center gap-3 hover:underline">
                         <img src="/icons/linkedin.png" alt="linkedin" className="w-6 h-6 filter dark:invert"/>
                         {t.linkedin}
                     </a>
 
-                    <a
-                        href="https://github.com/yuriisuprun"
+                    <a href="https://github.com/yuriisuprun"
                         target="_blank"
                         rel="noreferrer"
-                        className="flex items-center justify-center gap-3 hover:underline"
-                    >
+                        className="flex items-center justify-center gap-3 hover:underline">
                         <img src="/icons/github.png" alt="github" className="w-6 h-6 filter dark:invert"/>
                         {t.github}
                     </a>
