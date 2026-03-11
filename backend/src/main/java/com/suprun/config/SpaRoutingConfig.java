2d8fedce7a2fb109c71b2524e5211173e21f61df
yurii-suprun <iursuprun@gmail.com>
2026-03-11 11:13:54 +0100

    Update routing config

--- backend/src/main/java/com/suprun/config/SpaRoutingConfig.java
+++ backend/src/main/java/com/suprun/config/SpaRoutingConfig.java
@@ -1,17 +1,17 @@
-package com.suprun.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpaRoutingConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("forward:/index.html");
-        registry.addViewController("/about").setViewName("forward:/index.html");
-        registry.addViewController("/projects").setViewName("forward:/index.html");
-        registry.addViewController("/contacts").setViewName("forward:/index.html");
-    }
-}
+//package com.suprun.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpaRoutingConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/home").setViewName("forward:/index.html");
+//        registry.addViewController("/about").setViewName("forward:/index.html");
+//        registry.addViewController("/projects").setViewName("forward:/index.html");
+//        registry.addViewController("/contacts").setViewName("forward:/index.html");
+//    }
+//}
