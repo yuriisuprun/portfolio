bed5f0c546c9ddd6563b76e88bad67241c15e011
yurii-suprun <iursuprun@gmail.com>
2026-03-14 17:21:07 +0100

    Updated cors config

--- backend/src/main/java/com/suprun/config/CorsConfig.java
+++ backend/src/main/java/com/suprun/config/CorsConfig.java
@@ -10,15 +10,15 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*");
-
+                        .allowedOrigins("*") // for testing; replace * with frontend URL in production
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
