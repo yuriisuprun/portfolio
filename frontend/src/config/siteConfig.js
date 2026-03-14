170f7bbeb05fc9ebb432078264825ebe7031b805
yurii-suprun <iursuprun@gmail.com>
2026-03-14 14:12:27 +0100

    Add website config

---
+++ frontend/src/config/siteConfig.js
@@ -0,0 +1,7 @@
+export const SITE_CONFIG = {
+    name: "Yurii Suprun",
+    githubUsername: "yuriisuprun",
+    email: "iursuprun@gmail.com",
+    github: "https://github.com/yuriisuprun",
+    linkedin: "https://linkedin.com/in/yurii-suprun"
+};
