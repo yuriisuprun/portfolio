6e40cabf9c40cf1ba79753c2bca0874e298b01cb
yurii-suprun <iursuprun@gmail.com>
2026-03-14 21:30:24 +0100

    Update ContactController

--- backend/src/main/java/com/suprun/controller/ContactController.java
+++ backend/src/main/java/com/suprun/controller/ContactController.java
@@ -3,7 +3,10 @@ package com.suprun.controller;
 import com.suprun.dto.ContactRequest;
 import com.suprun.service.ContactService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/contact")
