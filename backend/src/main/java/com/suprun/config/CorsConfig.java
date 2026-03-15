def264215b4cf6e167329227150b9138bc34a8b5
yurii-suprun <iursuprun@gmail.com>
2026-03-15 13:20:06 +0100

    Update cors config

--- backend/src/main/java/com/suprun/config/CorsConfig.java
+++ backend/src/main/java/com/suprun/config/CorsConfig.java
@@ -1,44 +1,30 @@
-//package com.suprun.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//
-//        CorsConfiguration config = new CorsConfiguration();
-//
-//        config.setAllowCredentials(true);
-//
-//        config.setAllowedOrigins(Arrays.asList(
-//                "http://localhost:3000",
-//                "https://yuriisuprun.vercel.app"
-//        ));
-//
-//        config.setAllowedMethods(Arrays.asList(
-//                "GET",
-//                "POST",
-//                "PUT",
-//                "DELETE",
-//                "OPTIONS"
-//        ));
-//
-//        config.setAllowedHeaders(Arrays.asList("*"));
-//        config.setMaxAge(3600L);
-//
-//        UrlBasedCorsConfigurationSource source =
-//                new UrlBasedCorsConfigurationSource();
-//
-//        source.registerCorsConfiguration("/**", config);
-//
-//        return new CorsFilter(source);
-//    }
-//}
+package com.suprun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://yuriisuprun.vercel.app"
+        ));
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
