8d181c60158ecc2ebd603e653bbf7218680e63e5
yurii-suprun <iursuprun@gmail.com>
2026-03-20 12:01:22 +0100

    Add GITHUB TOKEN to github requests

--- backend/src/main/java/com/suprun/service/GithubService.java
+++ backend/src/main/java/com/suprun/service/GithubService.java
@@ -1,11 +1,16 @@
 package com.suprun.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GithubService {
+
+    @Value("${github.token}")
+    private String githubToken;
 
     @Cacheable("repos")
     public Object getRepos() {
@@ -14,6 +19,19 @@ public class GithubService {
 
         RestTemplate rest = new RestTemplate();
 
-        return rest.getForObject(url, Object.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + githubToken);
+        headers.set("Accept", "application/vnd.github+json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = rest.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                Object.class
+        );
+
+        return response.getBody();
     }
 }
