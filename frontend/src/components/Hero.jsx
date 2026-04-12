06f6ab8bf5c3c8a23465a9fa9f989a84f1e64c9c
yurii-suprun <iursuprun@gmail.com>
2026-04-12 12:24:47 +0200

    Update hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -118,13 +118,11 @@ function Hero({language}) {
                     {t("hero.roleSuffix")}
 
                     <span className="text-gray-700 dark:text-gray-300 font-semibold">
-            <Typewriter words={typewriterWords}
-                typingSpeed={90}
-                pauseDuration={5000}/>
-          </span>
 
-                    {t("hero.roleSuffixEnd")}
-                </p>
+                        <Typewriter words={typewriterWords} typingSpeed={90} pauseDuration={5000}/>
+                    </span>
+
+                    {t("hero.roleSuffixEnd")}</p>
 
                 <HeroLinks links={links}/>
             </div>
